576142d884229f5c6666b6289fcbbdb895bbeff0
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-10-23 12:05:00 +0200

    Creata component poesia

--- front-end/front-end-leopardi/src/app/app.module.ts
+++ front-end/front-end-leopardi/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PoesiaComponent } from './poesia/poesia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PoesiaComponent
   ],
   imports: [
     BrowserModule,
