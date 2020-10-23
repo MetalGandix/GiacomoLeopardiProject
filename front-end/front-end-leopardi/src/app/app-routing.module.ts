baef26d09439c6dabcb271ddbc65ad473354fb66
RiccardoPetracci <38490032+riccardo-petracci@users.noreply.github.com>
2020-10-23 12:16:19 +0200

    Chi-SIamo aggiunto alla toolbar

--- front-end/front-end-leopardi/src/app/app-routing.module.ts
+++ front-end/front-end-leopardi/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 import { HomeComponent } from './home/home.component';
 import { MappaComponent } from './mappa/mappa.component';
 import { PoesiaComponent } from './poesia/poesia.component';
@@ -7,7 +8,8 @@ import { PoesiaComponent } from './poesia/poesia.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'mappa', component: MappaComponent },
-  { path: 'poesia', component: PoesiaComponent }
+  { path: 'poesia', component: PoesiaComponent },
+  { path: 'chi-siamo', component: ChiSiamoComponent }
 ];
 
 @NgModule({
