2ab8679cf8d5c7f194f053960a52d666fb1d5749
RiccardoPetracci <38490032+riccardo-petracci@users.noreply.github.com>
2020-10-23 12:14:09 +0200

    Creata Componente ChiSiamo

--- front-end/front-end-leopardi/src/app/app.module.ts
+++ front-end/front-end-leopardi/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MappaComponent } from './mappa/mappa.component';
 import { PoesiaComponent } from './poesia/poesia.component';
+import { ChiSiamoComponent } from './chi-siamo/chi-siamo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MappaComponent,
-    PoesiaComponent
+    PoesiaComponent,
+    ChiSiamoComponent,
   ],
   imports: [
     BrowserModule,
