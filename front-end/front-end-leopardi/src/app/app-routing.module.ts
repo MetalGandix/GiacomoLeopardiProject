e505bdfcad5073e6ab5266d43cc21b0e8eba5ec9
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-10-23 12:12:40 +0200

    Aggiunti i routes

--- front-end/front-end-leopardi/src/app/app-routing.module.ts
+++ front-end/front-end-leopardi/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MappaComponent } from './mappa/mappa.component';
+import { PoesiaComponent } from './poesia/poesia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'mappa', component: MappaComponent },
+  { path: 'poesia', component: PoesiaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
