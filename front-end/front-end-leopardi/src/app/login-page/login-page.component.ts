1d905cdc2cd62126751525d79f21bdbdaf9a6416
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-08 14:32:52 +0100

    Login finito

--- front-end/front-end-leopardi/src/app/login-page/login-page.component.ts
+++ front-end/front-end-leopardi/src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../service/jwt-auth/authentication.service';
 
 @Component({
   selector: 'app-login-page',
@@ -9,15 +10,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(  private route: ActivatedRoute,
-    private router: Router) { }
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private loginservice: AuthenticationService) { }
 
   logEffettuato: boolean = true
+  username = ''
+  password = ''
+  invalidLogin = false
 
   ngOnInit(): void {
   }
 
-  effettuatoLogin(){
+  effettuatoLogin() {
     this.router.navigate(['/prenotazione-visita'], {
       state: {
         logEffettuato: this.logEffettuato = true
@@ -25,4 +30,22 @@ export class LoginPageComponent implements OnInit {
     })
   }
 
+  checkLogin() {
+    (this.loginservice.authenticate(this.username, this.password).subscribe(
+      data => {
+        this.invalidLogin = false
+        this.router.navigate(['/prenotazione-visita'], {
+          state: {
+            logEffettuato: this.logEffettuato = true
+          }
+        })
+      },
+      error => {
+        if (this.invalidLogin = true) {
+          alert("Il login è errato")
+        }
+      }
+    )
+    );
+  }
 }
