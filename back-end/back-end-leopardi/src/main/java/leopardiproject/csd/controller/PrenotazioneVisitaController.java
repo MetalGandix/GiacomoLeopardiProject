5107e0d0fbc7cbeda71a2a8d13ececb9c0cd5d5d
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-10 11:55:12 +0100

    Finito MVC dell'entità di PrenotazioneVisita

---
+++ back-end/back-end-leopardi/src/main/java/leopardiproject/csd/controller/PrenotazioneVisitaController.java
@@ -0,0 +1,42 @@
+package leopardiproject.csd.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import leopardiproject.csd.model.PrenotazioneVisita;
+import leopardiproject.csd.repository.PrenotazioneVisitaRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class PrenotazioneVisitaController {
+
+    @Autowired
+    private PrenotazioneVisitaRepository visitaRep;
+
+    // Metodo per inviare ad DB la visita con le info
+    @PostMapping("/visita")
+    String addUser(@RequestBody PrenotazioneVisita visita) {
+        visitaRep.save(visita);
+        return "Visita correttamente inviata";
+    }
+
+    // Metodo per vedere TUTTE le visite (Deve essere accessibile solo all'admin)
+    @GetMapping("/vediVisite")
+    public List<PrenotazioneVisita> getVisite() {
+        return (List<PrenotazioneVisita>) visitaRep.findAll();
+    }
+
+    // Metodo per vedere una singola visita dell'utente
+    @GetMapping("/vediVisita{visitaid}")
+    public PrenotazioneVisita vediVisitaSingola(@RequestBody PrenotazioneVisita visita) {
+        visitaRep.save(visita);
+        return visita;
+    }
+
+}
--- back-end/back-end-leopardi/src/main/java/leopardiproject/csd/model/PrenotazioneVisita.java
+++ back-end/back-end-leopardi/src/main/java/leopardiproject/csd/model/PrenotazioneVisita.java
@@ -1,5 +1,14 @@
 package leopardiproject.csd.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Entity
 @Table(name = "prenotazione")
 public class PrenotazioneVisita {
@@ -45,4 +54,116 @@ public class PrenotazioneVisita {
 
     @Column
     private boolean check3;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Date getOrario() {
+        return orario;
+    }
+
+    public void setOrario(Date orario) {
+        this.orario = orario;
+    }
+
+    public int getNumcomponenti() {
+        return numcomponenti;
+    }
+
+    public void setNumcomponenti(int numcomponenti) {
+        this.numcomponenti = numcomponenti;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getCellulare() {
+        return cellulare;
+    }
+
+    public void setCellulare(int cellulare) {
+        this.cellulare = cellulare;
+    }
+
+    public String getPaese() {
+        return paese;
+    }
+
+    public void setPaese(String paese) {
+        this.paese = paese;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public String getInfoAggiuntiva() {
+        return infoAggiuntiva;
+    }
+
+    public void setInfoAggiuntiva(String infoAggiuntiva) {
+        this.infoAggiuntiva = infoAggiuntiva;
+    }
+
+    public boolean isCheck1() {
+        return check1;
+    }
+
+    public void setCheck1(boolean check1) {
+        this.check1 = check1;
+    }
+
+    public boolean isCheck2() {
+        return check2;
+    }
+
+    public void setCheck2(boolean check2) {
+        this.check2 = check2;
+    }
+
+    public boolean isCheck3() {
+        return check3;
+    }
+
+    public void setCheck3(boolean check3) {
+        this.check3 = check3;
+    }
 }
