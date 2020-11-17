bf25fb5c9296cff4ce5a7f73615bc9ec3f99f627
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-17 22:26:51 +0100

    Aggiunte le nostre 3 email per fare le prove <3

--- back-end/back-end-leopardi/src/main/java/leopardiproject/csd/controller/PrenotazioneVisitaController.java
+++ back-end/back-end-leopardi/src/main/java/leopardiproject/csd/controller/PrenotazioneVisitaController.java
@@ -32,6 +32,8 @@ public class PrenotazioneVisitaController {
     @PostMapping("/visita")
     String addVisit(@RequestBody PrenotazioneVisita visita) throws MessagingException{
         smtpMailSender.send("leonardo.mogianesi@studenti.unicam.it", "Visita prenotata da " + visita.getCognome(), "La visita è stata prenotata da " + visita.getNome() + " \ned il numero di componenti è di: " + visita.getNumcomponenti() + " \nper il giorno: " + visita.getData() + " alle ore: " + visita.getOrario() + "." + "\nIl numero di cellulare del visitatore è: " + visita.getCellulare());
+        smtpMailSender.send("corrado.pallucchini@studenti.unicam.it", "Visita prenotata da " + visita.getCognome(), "La visita è stata prenotata da " + visita.getNome() + " \ned il numero di componenti è di: " + visita.getNumcomponenti() + " \nper il giorno: " + visita.getData() + " alle ore: " + visita.getOrario() + "." + "\nIl numero di cellulare del visitatore è: " + visita.getCellulare());
+        smtpMailSender.send("riccardo.petracci@studenti.unicam.it", "Visita prenotata da " + visita.getCognome(), "La visita è stata prenotata da " + visita.getNome() + " \ned il numero di componenti è di: " + visita.getNumcomponenti() + " \nper il giorno: " + visita.getData() + " alle ore: " + visita.getOrario() + "." + "\nIl numero di cellulare del visitatore è: " + visita.getCellulare());
         visitaRep.save(visita);
         otpController.sendOTP(visita.getCellulare());
         return "Visita correttamente inviata";
