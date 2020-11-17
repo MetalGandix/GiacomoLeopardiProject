97c67926021236af2b663a251604a129fe8721d1
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-17 14:42:04 +0100

    Aggiunte icone sulla sidebar

--- front-end/front-end-leopardi/src/app/sidenav/sidenav.component.ts
+++ front-end/front-end-leopardi/src/app/sidenav/sidenav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Output } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,12 +10,17 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent {
+  @Output() sidenavClose = new EventEmitter();
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+
+    public onSidenavClose = () => {
+      this.sidenavClose.emit();
+    }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
