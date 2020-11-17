46d3eb10900a3e39cae0e08f9740ced3996c664c
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-17 22:24:26 +0100

    Funziona la mail una volta prenotata la visita

--- back-end/back-end-leopardi/src/main/java/leopardiproject/csd/controller/PrenotazioneVisitaController.java
+++ back-end/back-end-leopardi/src/main/java/leopardiproject/csd/controller/PrenotazioneVisitaController.java
@@ -2,6 +2,8 @@ package leopardiproject.csd.controller;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import leopardiproject.csd.SmtpMailSender;
 import leopardiproject.csd.model.PrenotazioneVisita;
 import leopardiproject.csd.repository.PrenotazioneVisitaRepository;
 
@@ -21,9 +24,14 @@ public class PrenotazioneVisitaController {
     
     private OTPSystemController otpController;
 
+    @Autowired
+	private SmtpMailSender smtpMailSender;
+
     // Metodo per inviare al DB la visita con le info
+    // giacomoleopardi13@gmail.com
     @PostMapping("/visita")
-    String addUser(@RequestBody PrenotazioneVisita visita) {
+    String addVisit(@RequestBody PrenotazioneVisita visita) throws MessagingException{
+        smtpMailSender.send("leonardo.mogianesi@studenti.unicam.it", "Visita prenotata da " + visita.getCognome(), "La visita è stata prenotata da " + visita.getNome() + " \ned il numero di componenti è di: " + visita.getNumcomponenti() + " \nper il giorno: " + visita.getData() + " alle ore: " + visita.getOrario() + "." + "\nIl numero di cellulare del visitatore è: " + visita.getCellulare());
         visitaRep.save(visita);
         otpController.sendOTP(visita.getCellulare());
         return "Visita correttamente inviata";
