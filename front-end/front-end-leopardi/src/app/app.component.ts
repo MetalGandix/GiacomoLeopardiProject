fa8dd801c20a7084de53a92da01638ee8a768f65
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-11-17 12:51:56 +0100

    Sidenav finita

--- front-end/front-end-leopardi/src/app/app.component.ts
+++ front-end/front-end-leopardi/src/app/app.component.ts
@@ -11,27 +11,5 @@ export class AppComponent{
 
   title = 'front-end-leopardi';
 
-/*
-export class AppComponent implements OnInit {
-
-  title = 'front-end-leopardi';
-
-  private cookieValue: string;
-  value: string = this.cookieService.get('test');
-  cookieExists: boolean = this.cookieService.check('test');
-
-  constructor(private cookieService: CookieService) { }
-
-  public ngOnInit(): void {
-    this.cookieService.set('cookie-leopardi', 'out cookie value');
-    this.cookieValue = this.cookieService.get('cookie-leopardi');
-    this.cookieService.set('test', 'Hello World');
-    this.cookieService.set('test', 'Hello World', { expires: 2, sameSite: 'Lax' });
-
-    //ottenere cookie
-    alert("Presi cookies dell'utente ("+ this.cookieService.get("userid") + ") con successo")
-  }
-
-
-*/
+  showFiller = false;
 }
--- front-end/front-end-leopardi/src/app/app.module.ts
+++ front-end/front-end-leopardi/src/app/app.module.ts
@@ -38,6 +38,14 @@ import { AuthGuardComponent } from './service/jwt-auth/auth-guard/auth-guard.com
 import { AuthenticationService } from './service/authentication.service';
 import { BasicAuthHttpInterceptorService } from './service/basic-auth-http-interceptor.service';
 import { ContattiComponent } from './contatti/contatti.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -71,7 +79,8 @@ import { ContattiComponent } from './contatti/contatti.component';
     TariffaComponent,
     AutografoInnoComponent,
     AuthGuardComponent,
-    ContattiComponent
+    ContattiComponent,
+    SidenavComponent
   ],
   imports: [
     HttpClientModule, 
@@ -82,6 +91,13 @@ import { ContattiComponent } from './contatti/contatti.component';
     GoogleMapsModule,
     FormsModule,
     NgbModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [AuthGuardComponent,RoleGuardComponent,AuthenticationService,
     {
