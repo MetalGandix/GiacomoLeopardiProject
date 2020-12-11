265f398dbc261d106f17db7b3a13bdb63031c976
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-12-11 15:57:40 +0100

    Categorizzazione completata

--- front-end/front-end-leopardi/src/app/prenotazione-visita/prenotazione-visita.component.ts
+++ front-end/front-end-leopardi/src/app/prenotazione-visita/prenotazione-visita.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { stringify } from 'querystring';
 import { Prenotazione } from '../class/prenotazione';
 import { PrenotazioneService } from '../service/prenotazione.service';
 
@@ -20,6 +21,7 @@ export class PrenotazioneVisitaComponent implements OnInit {
   logRicevuto: boolean = false
   prenotazione: Prenotazione
   visitor: boolean = false
+  input: boolean = false
 
   ngOnInit(){
    this.visitor = sessionStorage.getItem("Role") === "ROLE_VISITATORE"
@@ -28,6 +30,20 @@ export class PrenotazioneVisitaComponent implements OnInit {
   onSubmit(){
     this.service.save(this.prenotazione).subscribe()
     this.logRicevuto = true
+  }
+
+  apriInput(){
+    this.input = true
+    console.log(this.input)
+  }
+
+  onChange(value: string){
+    if(value == " "){
+      this.input = true
+    }
+    else if(value == "Gruppi scolastici" || value == "Universitari" || value == "Associazioni culturali" || value == "Turisti"){
+      this.input = false
+    }
   }
 
 }
