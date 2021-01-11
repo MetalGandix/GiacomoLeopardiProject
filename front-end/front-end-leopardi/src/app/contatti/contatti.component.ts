51e4f6215b5d355ed7563164b504eab613c2bd1c
Corrado Pallucchini <corrado.pallucchini@studenti.unicam.it>
2021-01-11 16:53:27 +0100

    Eliminata mappa Contatti e vecchia mappa Google

--- front-end/front-end-leopardi/src/app/contatti/contatti.component.ts
+++ front-end/front-end-leopardi/src/app/contatti/contatti.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import  * as L from 'leaflet';
-import 'mapbox-gl-leaflet';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Contatti } from '../class/contatti';
 import { ContattiService } from '../service/contatti.service';
 
@@ -11,12 +9,8 @@ import { ContattiService } from '../service/contatti.service';
 })
 export class ContattiComponent implements OnInit, AfterViewInit {
 
-  private map: L.Map;
   messagge: Contatti
   booleanMessaggio: boolean = false 
-
-  @ViewChild('map')
-  private mapContainer: ElementRef<HTMLElement>;
 
   constructor(private service: ContattiService) { this.messagge = new Contatti() }
 
@@ -30,29 +24,6 @@ export class ContattiComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    const myAPIKey = "f1148686528a4ea488296c6f9f71041d";
-    const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
 
-    const initialState = {
-      lng: 11,
-      lat: 49,
-      zoom: 4
-    };
-
-    const map = new L.Map(this.mapContainer.nativeElement).setView(
-      [initialState.lat, initialState.lng],
-      initialState.zoom
-    );
-
-    map.attributionControl
-      .setPrefix("")
-      .addAttribution(
-        'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
-      );
-
-    L.mapboxGL({
-      style: `${mapStyle}?apiKey=${myAPIKey}`,
-      accessToken: "no-token"
-    }).addTo(map);
   }
 }
--- front-end/front-end-leopardi/src/app/mappa/mappa.component.ts
+++ front-end/front-end-leopardi/src/app/mappa/mappa.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -8,8 +7,6 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./mappa.component.css']
 })
 export class MappaComponent implements OnInit {
-  @ViewChild(GoogleMap, { static: false }) map: GoogleMap
-  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow
 
   constructor(
     private route: ActivatedRoute,
@@ -20,18 +17,6 @@ export class MappaComponent implements OnInit {
   bottoneCliccato: boolean = false
   visita: boolean
   aggiunta: boolean
-  zoom = 25
-  center: google.maps.LatLngLiteral
-  options: google.maps.MapOptions = {
-    zoomControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    mapTypeId: 'hybrid',
-    maxZoom: 60,
-    minZoom: 8,
-  }
-  markers = []
-  infoContent = ''
 
   visitaGuidataMethod() {
     this.visita = true
@@ -46,203 +31,6 @@ export class MappaComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.router.navigateByUrl('/mappa', { skipLocationChange: true }).then(() => {
-      this.router.navigate(['/mappa']);
-    });
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      }
-      this.markers.push({
-        position: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        },
-        label: {
-          color: 'red',
-          text: 'La mia posizione',
-        },
-      })
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398125,
-        lng: 13.551801,
-      },
-      label: {
-        color: 'red',
-        text: 'Casa Leopardi ' + (this.markers.length + 1),
-      },
-      title: 'Casa Leopardi' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398118,
-        lng: 13.549539,
-      },
-      label: {
-        color: 'red',
-        text: 'Casa Adelaide ' + (this.markers.length + 1),
-      },
-      title: 'Casa Adelaide' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398031,
-        lng: 13.551082,
-      },
-      label: {
-        color: 'red',
-        text: 'Centro Nazionale Studi Leopardiano ' + (this.markers.length + 1),
-      },
-      title: 'Centro Nazionale Studi Leopardiano' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.397976,
-        lng: 13.552959,
-      },
-      label: {
-        color: 'red',
-        text: 'Chiesa dei Cappuccini ' + (this.markers.length + 1),
-      },
-      title: 'Chiesa dei Cappuccini ' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398118,
-        lng: 13.549539,
-      },
-      label: {
-        color: 'red',
-        text: "Colle dell'infinito " + (this.markers.length + 1),
-      },
-      title: "Colle dell'infinito " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398014,
-        lng: 13.551883,
-      },
-      label: {
-        color: 'red',
-        text: "La lettera della Befana " + (this.markers.length + 1),
-      },
-      title: "La lettera della Befana " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398037,
-        lng: 13.549731,
-      },
-      label: {
-        color: 'red',
-        text: "Verso il Monte Tabor " + (this.markers.length + 1),
-      },
-      title: "Verso il Monte Tabor " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.402725,
-        lng: 13.550005,
-      },
-      label: {
-        color: 'red',
-        text: "Palazzo Venieri " + (this.markers.length + 1),
-      },
-      title: "Palazzo Venieri " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.403816,
-        lng: 13.548187,
-      },
-      label: {
-        color: 'red',
-        text: "Piazza della Torre del Borgo " + (this.markers.length + 1),
-      },
-      title: "Piazza della Torre del Borgo " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398014,
-        lng: 13.551883,
-      },
-      label: {
-        color: 'red',
-        text: "Piazzuola del Sabato del Villaggio " + (this.markers.length + 1),
-      },
-      title: "Piazzuola del Sabato del Villaggio " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.402059,
-        lng: 13.551869,
-      },
-      label: {
-        color: 'red',
-        text: "La Torre del Passero Solitario " + (this.markers.length + 1),
-      },
-      title: "La Torre del Passero Solitario " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.401622,
-        lng: 13.552179,
-      },
-      label: {
-        color: 'red',
-        text: "Via Roma " + (this.markers.length + 1),
-      },
-      title: "Via Roma " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
-    this.markers.push({
-      position: {
-        lat: 43.398055,
-        lng: 13.549608,
-      },
-      label: {
-        color: 'red',
-        text: "La vista sui Monti Azzurri " + (this.markers.length + 1),
-      },
-      title: "La vista sui Monti Azzurri " + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-    })
+    
   }
-
-  zoomIn() {
-    if (this.zoom < this.options.maxZoom) this.zoom++
-  }
-
-  zoomOut() {
-    if (this.zoom > this.options.minZoom) this.zoom--
-  }
-
-  click(event: google.maps.MouseEvent) {
-    console.log(event)
-  }
-
-  logCenter() {
-    console.log(JSON.stringify(this.map.getCenter()))
-  }
-
-  openInfo(marker: MapMarker, content) {
-    this.infoContent = content
-    this.info.open(marker)
-  }
-
-
 }
