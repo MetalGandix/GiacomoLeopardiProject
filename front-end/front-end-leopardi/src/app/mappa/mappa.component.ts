35906235cead0ef9cca24f7edfb743f690fe9355
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2021-01-27 12:09:42 +0100

    Vai petrac

--- front-end/front-end-leopardi/src/app/mappa/mappa.component.ts
+++ front-end/front-end-leopardi/src/app/mappa/mappa.component.ts
@@ -13,7 +13,8 @@ export class MappaComponent implements OnInit, AfterViewInit {
 
   @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
-  private mappa
+  mappa
+  private map
   constructor( private http: HttpClient) {
 
   }
@@ -28,12 +29,12 @@ export class MappaComponent implements OnInit, AfterViewInit {
       zoom: 15
     };
 
-    const map = new L.Map(this.mapContainer.nativeElement).setView(
+    this.map = new L.Map(this.mapContainer.nativeElement).setView(
       [initialState.lat, initialState.lng],
       initialState.zoom
     );
 
-    map.attributionControl
+    this.map.attributionControl
       .setPrefix("")
       .addAttribution(
         'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
@@ -42,7 +43,7 @@ export class MappaComponent implements OnInit, AfterViewInit {
     L.mapboxGL({
       style: `${mapStyle}?apiKey=${myAPIKey}`,
       accessToken: "no-token"
-    }).addTo(map);
+    }).addTo(this.map);
   }
 
   bottoneCliccato: boolean = false
@@ -67,7 +68,7 @@ export class MappaComponent implements OnInit, AfterViewInit {
      this.http.get<any>('https://api.geoapify.com/v1/routing?waypoints=43.3981713,13.5519596|43.3981415,13.5528702|43.397988308481956,13.551873093784934|43.39803201295075,13.551656134928862|43.39794916747729,13.551186681668128|43.3973867596313,13.550378307255414|43.39771624888235,13.550188073299523|43.39809170402481,13.549666815359387|43.39962677234598,13.552603069518|43.400688829187175,13.552380016680672|43.40182973979836,13.552144502578813|43.40239377083367,13.551448424660066|43.40250032674169,13.549999971909983&mode=walk&lang=it&apiKey=f1148686528a4ea488296c6f9f71041d')
      .subscribe(calculatedRouteGeoJSON => {
        this.mappa = calculatedRouteGeoJSON
-      	console.log("MAPPA",calculatedRouteGeoJSON);
+        console.log("MAPPA",calculatedRouteGeoJSON);
       })
    }
-};
+}
