4eef2627899c066bd9c0ede936cab780d9a3ddb6
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2021-02-06 12:44:56 +0100

    QUESTO PUSH è PER IL DEPLOY

--- back-end/back-end-leopardi/src/main/java/leopardiproject/csd/CsdApplication.java
+++ back-end/back-end-leopardi/src/main/java/leopardiproject/csd/CsdApplication.java
@@ -2,9 +2,16 @@ package leopardiproject.csd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CsdApplication {
+public class CsdApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CsdApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsdApplication.class, args);
